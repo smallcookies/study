5e34b547137f31f50f81051f03d5db2887c32249
smallcookies <369458018@qq.com>
2019-11-18 20:47:21 +0800

    kuorong

---
+++ TestDemo1.java
@@ -0,0 +1,33 @@
+package homework;
+/*扩容：
+arr = Arrays.copyof(arr,arr.length+1)
+缩容：
+arr = Arrays.copyof(arr,arr.length-1)
+ */
+import java.util.Arrays;
+
+public class TestDemo1 {
+
+//public static void main(String[] args) {
+//
+//        int[] arr = new int[] {1,2,3};
+//
+//        System.out.println(Arrays.toString(add(arr, 9)));
+//
+//        }
+//
+//     public static int[] add(int[] arr, int num) {
+//
+//        int [] result = new int[arr.length+1];
+//
+//        for(int i=0;i<arr.length;i++) {
+//
+//        result[i]=arr[i];
+//        }
+//        result[result.length-1] = num;
+//
+//        return result;
+//
+//            }
+
+      }
