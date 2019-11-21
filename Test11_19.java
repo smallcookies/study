eb2d58339da5d86316e270a1417fffba9c1f0801
smallcookies <369458018@qq.com>
2019-11-21 18:49:55 +0800

    jeicheng and caishuzi

---
+++ Test11_19.java
@@ -0,0 +1,78 @@
+package Classwork;
+
+import com.sun.org.apache.bcel.internal.generic.RET;
+
+public class Test11_19 {
+//// 猜数字（整数类型1-100）
+//    public static void main(String[] args) {
+//        int i = 32;
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("请输入数字：");
+//        int a = sc.nextInt();
+//            if (a < i) {
+//                System.out.println("数字小了!");
+//            } else if (a > i) {
+//                System.out.println("数字大了！");
+//            } else if (a == i) {
+//                System.out.println("输入正确！恭喜你！");
+//            }
+//    }
+
+
+
+//    public static void main(String[] args) {
+//
+//    }
+//
+//    public static void jiechen(int x) {
+//        long sum = 0;
+//        int i = 1;
+//        int n =0;
+//        for (i= 1;i<n;i++){
+//            sum *=1;
+//        }
+//    }
+
+
+
+
+//// 若一个方法用void声明，在方法体中也可以使用return来结束本方法的调用。
+////不带返回值的方法
+//    public static void test(int num) {
+//        if (num == 2){
+//            //结束方法调用
+//            return;
+//        }
+//        System.out.println();
+//    }
+
+
+
+
+
+//    //重载方法一定和返回值无关
+//    //方法重载:
+//    public static void main(String[] args) {
+//        double a = 1.0;
+//        double b = 2.0;
+//        double ret = add(a,b);
+//        int c = 3;
+//        int d = 4;
+//        int num =add(c,d);
+//        System.out.println(ret);
+//        System.out.println(num);
+//    }
+//
+//    public static int add(int x,int y) {
+//        return x+y;
+//    }
+//    public static double add(double x,double y) {
+//        return x+y;
+//    }
+
+
+
+
+
+    //
+}
