2cacd060049166aa8a4d96517b673d95adc3ee05
smallcookies <369458018@qq.com>
2019-11-21 18:58:33 +0800

    age

---
+++ Test11_21.java
@@ -0,0 +1,51 @@
+package homework;
+import java.util.Scanner;
+public class Test11_21 {
+//// 1. 根据年龄, 来打印出当前年龄的人是少年(低于18), 青年(19-28), 中年(29-55), 老年(56以上)
+//    public static void main(String[] args) {
+//        while (true) {
+//            Scanner in = new Scanner(System.in);
+//            System.out.println("请输入你所要查询的年龄：");
+//            int num = in.nextInt();
+//            if (num < 0) {
+//                System.out.println("请输入正确的年龄");
+//                break;
+//            } else if (num >= 0 && num <= 18) {
+//                System.out.println("少年");
+//                break;
+//            } else if (num >= 19 && num <= 28) {
+//                System.out.println("青年");
+//                break;
+//            } else if (num >= 29 && num <= 55) {
+//                System.out.println("中年");
+//                break;
+//            } else if (num >= 56) {
+//                System.out.println("老年");
+//                break;
+//            }
+//        }
+//    }
+
+
+
+
+
+//  //2. 判定一个数字是否是素数
+//public static void main(String[] args) {
+//    int num = 4;
+//    int i = 2;
+//    for (i = 2; i < num; i++) {
+//        //只要能被其他数整除则这个数就肯定不是素数
+//        if (num % i == 0) {
+//            System.out.println(num + " 不是素数");
+//            break;
+//        }
+//    }//跳出循环的条件要么不是素数,要么是 i 已经等于本身或者这个数小于2.
+//    if (i == num) {
+//        System.out.println(num + " 是素数");
+//    } else if (num <= 1){
+//        System.out.println("您的数字有误!");
+//    }
+//  }
+
+    }
