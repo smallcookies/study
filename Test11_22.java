c6312c7426ae424b223c633b9f135373ff549332
smallcookies <369458018@qq.com>
2019-11-22 21:26:51 +0800

    year

---
+++ Test11_22.java
@@ -0,0 +1,42 @@
+package homework;
+
+public class Test11_22 {
+    //    //3. 打印 1 - 100 之间所有的素数
+//    public static void main(String[] args) {
+//        int a = 0 ;
+//        for (int i = 2;i <= 100;i++){
+//            boolean flag = true;
+//            for (int j = 2;j < i;j++){
+//                if (i%j == 0){
+//                    flag = false;
+//                    break;
+//                }
+//            }
+//            if (flag){
+//                a++;
+//                System.out.println(i+"");
+//            }
+//            if(a%10==0) {
+//                System.out.println();
+//            }
+//        }
+//    }
+
+
+
+
+
+//    //4. 输出 1000 - 2000 之间所有的闰年
+//    public static void main(String[] args) {
+//        int year;
+//        for ( year= 1000; year <= 2000;year++){
+//            if (year%400 == 0){              //世纪闰年
+//                System.out.printf(year+" ");
+//            }
+//            else if (year%4 == 0 && year%100 != 0){      //普通闰年
+//                System.out.printf(year+" ");
+//            }
+//        }
+//    }
+    
+}
