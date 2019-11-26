08b86c9c08fc816c0f4fc65b292e9abfc8a6c0cb
smallcookies <369458018@qq.com>
2019-11-26 16:26:26 +0800

    value

---
+++ Test11_26.java
@@ -0,0 +1,45 @@
+package homework;
+
+public class Test11_26 {
+    ////(1)12. 获取一个数二进制序列中所有的偶数位和奇数位， 分别输出二进制序列。
+//public static void main(String[] args) {
+//    int value = 11;
+//    int[] arr1 = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+//    int[] arr2 = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+//    int j=0;
+//    for(int i=0;i<32;i+=2){
+//        arr1[j]=(value>>i)&1;
+//        j++;
+//    }
+//        j=0;
+//    for(int i=1;i<32;i+=2){
+//        arr2[j]=(value>>i)&1;
+//        j++;
+//    }
+//    for(int i=15;i>=0;i--){
+//        System.out.print(arr1[i]);
+//    }
+//    System.out.println("");
+//
+//    for(int i=15;i>=0;i--){
+//        System.out.print(arr2[i]);
+//    }
+//  }
+//   // (2)12. 获取一个数二进制序列中所有的偶数位和奇数位， 分别输出二进制序列。
+//    //  以11为例子，
+//    // 11：0000 0000 0000 0000 0000 0000 0000 1011
+//    // 奇数位序列：0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1
+//    // 偶数位序列：0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1
+//     public static void main(String[] args) {
+//         int value=11;
+//         System.out.println("偶数序列");
+//         for(int i=31;i>0;i-=2){
+//             System.out.print((value>>i)&1);
+//         }
+//         System.out.println("");
+//         System.out.println("奇数序列");
+//         for(int i=30;i>=0;i-=2){
+//             System.out.print((value>>i)&1);
+//         }
+//     }
+}
