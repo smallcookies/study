0708aaa4d6265b465d3870b7a349b336d7071f37
smallcookies <369458018@qq.com>
2019-11-29 16:11:27 +0800

    copy

---
+++ Test11_29.java
@@ -0,0 +1,45 @@
+package homework;
+
+public class Test11_29 {
+    //深拷贝
+//public static void main(String[] args) {
+//    int[] arr = {1,2,3,4,5,6};
+//    //产生一个新数组，新数组的内容与原数组相同
+//    //新数组是一块新的空间，深拷贝
+//    int[] newArr = Arrays.copyOf(arr, arr.length);
+//    System.out.println("newArr: " + Arrays.toString(newArr));
+//    arr[0] = 10;
+//    System.out.println("arr: " + Arrays.toString(arr));
+//    System.out.println("newArr: " + Arrays.toString(newArr));
+//}
+
+//                   //  浅拷贝
+//public static void main(String[] args) {
+//    int[] arr = {1,2,3,4,5,6};
+//    int[] newArr = arr;
+//    System.out.println("newArr: " + Arrays.toString(newArr));
+//    arr[0] = 10;
+//    System.out.println("arr: " + Arrays.toString(arr));
+//    System.out.println("newArr: " + Arrays.toString(newArr));
+//}
+
+
+
+
+
+
+// //   写一个方法，将数组中的每个元素都  * 2
+// public static void main(String[] args) {
+//     int [] arr = {1,2,3,4};
+//     System.out.println(Arrays.toString(arr));
+//     doubleArray(arr);
+//     System.out.println(Arrays.toString(arr));
+// }
+// public static void doubleArray(int [] array) {
+//     for (int i = 0;i < array.length;i++){
+//         array [i] *= 2;
+//     }
+//     System.out.println(array);
+// }
+
+}
