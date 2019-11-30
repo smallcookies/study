b84a99d8ccba7c5edde0f7188568cc6f7d29c54d
smallcookies <369458018@qq.com>
2019-11-30 09:47:24 +0800

    over loading

---
+++ Test11_26_1.java
@@ -0,0 +1,64 @@
+package JAVA11_26;
+
+public class Test11_26_1 {
+//    //调用函数计算两个整数相加
+//    public static void main(String[] args) {
+//        int a= 10;int b = 20;
+//        int c = add(a,b);
+//        System.out.println("a和b的和为："+c);
+//    }
+//
+//    public static int add(int x,int y)  {
+//        System.out.println("调用方法中的x值为："+ x +"  y值为：" +y);
+//        return x+y;
+//    }
+
+
+
+//    //计算n的阶乘
+//    public static void main(String[] args) {
+//        int n = 5;
+//        int a = jiecheng(5);
+//        System.out.println("n的阶乘为："+a);
+//    }
+//
+//    public static int jiecheng(int n) {
+//        int result = 1;
+//        for (int i = 1;i<= n;i++ ){
+//            result *= i;
+//        }
+//        return result;
+//    }
+
+
+
+
+
+
+
+    //    //重载方法一定和返回值无关
+//    //方法重载:
+//    public static void main(String[] args) {
+//        double a = 1.0;
+//        double b = 2.0;
+//        double ret = add(a,b);
+//        int c = 3;
+//        int d = 4;
+//        int num =add(c,d);
+//        System.out.println(ret);
+//        System.out.println(num);
+//    }
+//
+//    public static int add(int x,int y) {
+//        return x+y;
+//    }
+//    public static double add(double x,double y) {
+//        return x+y;
+//    }
+
+
+
+
+
+    //
+}
