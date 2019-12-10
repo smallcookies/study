8726b1d8d0d190890e108bd682c485480ce8860b
smallcookies <369458018@qq.com>
2019-12-10 16:15:36 +0800

    Solution21

---
+++ Solution21.java
@@ -0,0 +1,28 @@
+package oj;
+
+//     将两个有序链表合并为一个新的有序链表并返回。
+//     新链表是通过拼接给定的两个链表的所有节点组成的
+public class Solution21 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode resNode = new ListNode(0);
+        ListNode cur = resNode;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val){
+                cur.next = l1;
+                cur = cur.next;
+                l1 = l1.next;
+            }else {
+                cur.next = l2;
+                cur = cur.next;
+                l2 = l2.next;
+            }
+        }
+        if (l1 != null) {
+            cur.next = l1;
+        }
+        if (l2 != null) {
+            cur.next = l2;
+        }
+        return resNode.next;
+    }
+}
