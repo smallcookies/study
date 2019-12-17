c7013de12636e160b4137d374563bf27d7f3bfb4
smallcookies <369458018@qq.com>
2019-12-17 16:34:15 +0800

    Solution141

---
+++ Solution141.java
@@ -0,0 +1,29 @@
+package oj;
+
+//给定一个链表，判断链表中是否有环。
+//
+//  为了表示给定链表中的环，
+// 我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。
+// 如果 pos 是 -1，则在该链表中没有环。
+//   示例 1：
+//
+//   输入：head = [3,2,0,-4], pos = 1
+//   输出：true
+//   解释：链表中有一个环，其尾部连接到第二个节点。
+
+public class Solution141 {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode low = head, fast = head;
+        while (fast != null && fast.next != null) {
+            low = low.next;
+            fast = fast.next.next;
+            if (low == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
